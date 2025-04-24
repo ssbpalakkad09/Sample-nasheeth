28cea176d49f37b72bdaadc670f49194dcb64323
ssbpalakkad09 <ssbitcellpalakkad@gmail.com>
2025-04-24 12:21:12 +0530

    changed

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 print("Hai")
+
+class Animal:
+    def __init__(self, name):
+        self.name = name    
+        self.sound = None
+        self.age = 0
+
+        def make_sound(self):
+        if self.sound:
+            print(f"{self.name} says {self.sound}")
+        else:
+            print(f"{self.name} does not make a sound")
+
+            #changes made by vishnu
